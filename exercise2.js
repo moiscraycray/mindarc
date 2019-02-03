026cf5345634dd70f10e863109126dcc53d1719b
Olivia Mo <oliviatrademark@gmail.com>
2019-02-03 18:54:31 +1100

    add exercise 2 files

---
+++ exercise2.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $('.b-accordian-container div').not(':first-child').hide();
+});
+
+// $("li:not(:first-child)").addClass("something");
+// $('#collapse tr').not(":first-child").hide();
